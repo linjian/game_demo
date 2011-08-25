a55b451e4a55dffc313fd2bca1787e413218bd6c
linjian <linjian@linjian-desktop.(none)>
2011-08-25 18:14:38 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameDemo::Application.config.secret_token = '45112bc97c80b94313c673ce376910ae0725a600944bd15f33233b884bcf90cae9869a9064caced3dddf615491e96738f03a9adba80fd0fc595e679cfccdbff6'
